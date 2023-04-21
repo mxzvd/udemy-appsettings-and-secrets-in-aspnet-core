5884616ef8a4cab6c25cdb9d80d04ea16dda7eea
Maxim Zavidei <maxim-zavidei+git@pm.me>
2023-04-21 22:43:47 +0200

    Created initial mvc app and added it to the sln

---
+++ src/AppSettingsManager/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AppSettingsManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
